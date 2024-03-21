25498ded64a6af8961324906fcfe5841829f3557
Karolina Slemp-Rejowska <slempkarolina@gmail.com>
2024-03-21 11:05:54 +0100

    Add Navigation

--- app/src/main/java/com/karolina/jetpack/newsapp/MainActivity.kt
+++ app/src/main/java/com/karolina/jetpack/newsapp/MainActivity.kt
@@ -1,46 +1,37 @@
 package com.karolina.jetpack.newsapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.karolina.jetpack.newsapp.domain.usecases.app_entry.AppEntryUseCases
-import com.karolina.jetpack.newsapp.presentation.onboarding.OnBoardingScreen
-import com.karolina.jetpack.newsapp.presentation.onboarding.OnBoardingViewModel
+import com.karolina.jetpack.newsapp.presentation.navgraph.NavGraph
 import com.karolina.jetpack.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var useCase: AppEntryUseCases
-
+    val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        installSplashScreen()
-
-        lifecycleScope.launch {
-            useCase.readAppEntry().collect {
-                Log.d("Test", it.toString())
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.splashCondition
             }
         }
+
         setContent {
             NewsAppTheme(dynamicColor = false) {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    val viewModel: OnBoardingViewModel = hiltViewModel()
-                    OnBoardingScreen(onEvent = { viewModel.onEvent(it) })
+                    val startDestination = viewModel.startDestination
+                    NavGraph(startDestination = startDestination)
                 }
             }
         }
---
+++ app/src/main/java/com/karolina/jetpack/newsapp/presentation/navgraph/NavGraph.kt
@@ -0,0 +1,40 @@
+package com.karolina.jetpack.newsapp.presentation.navgraph
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.karolina.jetpack.newsapp.presentation.onboarding.OnBoardingScreen
+import com.karolina.jetpack.newsapp.presentation.onboarding.OnBoardingViewModel
+
+@Composable
+fun NavGraph(
+    startDestination: String
+) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = startDestination) {
+        navigation(
+            route = Route.AppStartNavigation.route,
+            startDestination = Route.OnBoardingScreen.route
+        ) {
+            composable(
+                route = Route.OnBoardingScreen.route
+            ) {
+                val viewModel: OnBoardingViewModel = hiltViewModel()
+                OnBoardingScreen(onEvent = { viewModel.onEvent(it) })
+            }
+        }
+
+        navigation(
+            route = Route.NewsNavigation.route,
+            startDestination = Route.NewsNavigatorScreen.route
+        ){
+            composable(route = Route.NewsNavigatorScreen.route) { 
+                Text(text = "News Navigator Screen")
+            }
+        }
+    }
+}
