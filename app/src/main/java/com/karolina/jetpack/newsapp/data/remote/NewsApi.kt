3be81ef89637549addbf68f88d941d5ced02614d
Karolina Slemp-Rejowska <slempkarolina@gmail.com>
2024-03-21 12:40:54 +0100

    Connection with news API, PagingData

---
+++ app/src/main/java/com/karolina/jetpack/newsapp/data/remote/NewsApi.kt
@@ -0,0 +1,15 @@
+package com.karolina.jetpack.newsapp.data.remote
+
+import com.karolina.jetpack.newsapp.data.remote.dto.NewsResponse
+import com.karolina.jetpack.newsapp.util.Constants.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+    @GET("everything")
+    suspend fun getNews(
+        @Query("page") page: Int,
+        @Query("sources") sources: String,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): NewsResponse
+}
---
+++ app/src/main/java/com/karolina/jetpack/newsapp/data/repository/NewsRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.karolina.jetpack.newsapp.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.karolina.jetpack.newsapp.data.remote.NewsApi
+import com.karolina.jetpack.newsapp.data.remote.NewsPagingSource
+import com.karolina.jetpack.newsapp.domain.model.Article
+import com.karolina.jetpack.newsapp.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
+
+class NewsRepositoryImpl(
+    private val newsApi: NewsApi
+) : NewsRepository {
+    override fun getNews(sources: List<String>): Flow<PagingData<Article>> {
+        return Pager(
+            config = PagingConfig(pageSize = 10),
+            pagingSourceFactory = {
+                NewsPagingSource(
+                    newsApi = newsApi,
+                    sources = sources.joinToString(separator = ",")
+                )
+            }
+        ).flow
+    }
+}
